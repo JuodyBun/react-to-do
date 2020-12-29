aab8cfebc955de7c3fe4eba1a3d9fc984814af6c
Juody Bun <bjuody@gmail.com>
2020-12-28 22:26:50 -0600

    Finished App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function Todo({ todo }) { //component that is used to return to the main App component
@@ -20,8 +19,9 @@ function App() {
   return (
     <div className="app">
       <div className="todo-list">
-        {todos.map((todo, index) => (
-          <Todo
+        {todos.map((todo, index) => ( //by using map(), new array of items is created by mapping over the todo items from state
+        //and displaying them by index
+          <Todo //^this adds a <div> for app, a <div> for todo-list, and a map of the todos to Todo components
             key={index}
             index={index}
             todo={todo}
